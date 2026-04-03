68e791dc581b199ba114f3c078014e8f178fbfb2
Ana Zahra <anazahratulfirdausi@gmail.com>
2026-04-03 20:22:55 +0700

    add MODUL 9

---
+++ MODUL 9/src/GUI.java
@@ -0,0 +1,8 @@
+public class GUI { 
+    public static void main(String[] args) {
+        TFrame window = new TFrame();
+        window.setTitle("Hitung Nilai Akhir dengan GUI java.awt");
+        window.setSize(400, 300);
+        window.show();
+    }
+}
